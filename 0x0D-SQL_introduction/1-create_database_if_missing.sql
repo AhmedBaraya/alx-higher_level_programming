03efa203712555c84f6a9aef1e9212fa036dad7d
AhmedBaraya <ahmedbaraya68@gmail.com>
2024-05-22 11:43:20 -0700

    sqlc

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,11 @@
---Create the Database hbtn_0c_0 .
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0; 
+-- Connect to MySQL server
+mysql -u your_username -p
+
+-- Enter your MySQL password when prompted
+
+-- Create the database
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+
+-- Exit MySQL
+exit
+
